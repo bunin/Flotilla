bafc24f15435fc963999b99d181d8c463f8ae918
David Gevorkyan <dgevorkyan@eharmony.com>
2015-04-13 18:55:07 -0700

    Still getting problems with sarama library, want to ensure that there are no dependencies trying to pull from "tylertreat" git repo.

--- flotilla-server/daemon/broker/pubsub/pubsub.go
+++ flotilla-server/daemon/broker/pubsub/pubsub.go
@@ -9,7 +9,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/cloud/pubsub"
 
-	"github.com/tylertreat/Flotilla/flotilla-server/daemon/broker"
+	"github.com/davidgev/Flotilla/flotilla-server/daemon/broker"
 )
 
 const (
